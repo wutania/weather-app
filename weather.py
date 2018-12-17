58560fe9b9529262196781e794b2aa780d5ff12f
Tania Tan Wu <wutania@gmail.com>
2018-12-17 09:58:08 -0800

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+import os
+import pytz
+import requests
+import math
+API_KEY = '01f15f873de800511a72e6f2bbb9631e'
+API_URL = ('http://api.openweathermap.org/data/2.5/weather?q={}&mode=json&units=metric&appid={}')
+def query_api(city):
+    try:
+        print(API_URL.format(city, API_KEY))
+        data = requests.get(API_URL.format(city, API_KEY)).json()
+    except Exception as exc:
+        print(exc)
+        data = None
+    return data
